d8088e6e480d5da7905213dc2246e2a905df2bef
sd-deep <shankhadeepdas8@gmail.com>
2020-05-14 22:13:04 +0530

    Updates

---
+++ precache-manifest.ae9dd23aa647165a9c5ba24948e8365b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424b910ddcca1d4773b9d73fce02834f",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "d25bcc6f704ab05e5638",
+    "url": "/covid19-tracker/static/css/main.22cccdeb.chunk.css"
+  },
+  {
+    "revision": "9a1d20d708bf9493c986",
+    "url": "/covid19-tracker/static/js/2.79b9bc97.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.79b9bc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25bcc6f704ab05e5638",
+    "url": "/covid19-tracker/static/js/main.543682be.chunk.js"
+  },
+  {
+    "revision": "d129c7318b69f0e81f66",
+    "url": "/covid19-tracker/static/js/runtime-main.c77f150a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/covid19.d7265326.png"
+  },
+  {
+    "revision": "642ef1c719858beda68aa44b2a7a0e3b",
+    "url": "/covid19-tracker/static/media/undraw_medical_research_qg4d.642ef1c7.svg"
+  },
+  {
+    "revision": "0ffc67ee4d765d3dcf230a1090dece32",
+    "url": "/covid19-tracker/static/media/undraw_social_distancing_2g0u.0ffc67ee.svg"
+  },
+  {
+    "revision": "80d7cc003d4b826fb320beaddf56c074",
+    "url": "/covid19-tracker/static/media/undraw_wash_hands_nwl2.80d7cc00.svg"
+  }
+]);
